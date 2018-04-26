c82b3347df66fdc0b2eb62a5d523dfa9ce65b153
Francisco Granado Mota Garcia <a54810@alunos.uminho.pt>
2018-04-26 15:27:32 +0100

    classe GestaoContribuintes

---
+++ GestaoContribuintes.java
@@ -0,0 +1,56 @@
+
+/**
+ * Escreva a descrição da classe GestaoContribuintes aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+public class GestaoContribuintes {
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+  private Map<Integer, Contribuinte> contribuintes;
+
+  /**
+  * COnstrutor para objetos da classe GestaoContribuintes
+  */
+  public GestaoContribuintes(){
+    this.contribuintes = new HashMap<>();
+  }
+
+  public GestaoContribuintes(Map<Integer,Contribuinte> cont) {
+    //this.encomendas = encs.values().stream().collect(Collectors.toMap((e) -> e.getNEnc(),(e) -> e.clone(),(e1,e2) -> e1,HashMap::new));
+    this.contribuintes = cont.values().stream().collect(Collectors.toMap((c) -> c.getNif(),(c) -> c.clone()));
+  }
+  
+  public GestaoContribuintes(GestaoContribuintes gc) {
+    this.contribuintes = gc.getContribuintes();    
+  }
+  
+  public Map<Integer,Contribuinte> getContribuintes() {
+    return this.contribuintes.values().stream().collect(Collectors.toMap((c) -> c.getNif(),(c) -> c.clone())); 
+  }
+  
+  public String toString() {
+     StringBuffer sb = new StringBuffer();
+     for (Contribuinte c: this.contribuintes.values())
+       sb.append(c.toString() + "\n");
+     return sb.toString(); 
+  }
+   
+  public boolean equals(Object o) {
+    if (this == o) 
+      return true;
+    if ((o == null) || (this.getClass() != o.getClass()))
+      return false;
+    GestaoContribuintes gc = (GestaoContribuintes) o;
+    return this.contribuintes.equals(gc.getContribuintes());
+       
+   }    
+   
+  public GestaoContribuintes clone() {
+     return new GestaoContribuintes(this); 
+   }
+}
